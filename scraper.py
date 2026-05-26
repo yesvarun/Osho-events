6e1771078dec21b811ed50e4fc2b27f14d0f6f6e
yesvarun <yesvarun@gmail.com>
2026-05-26 20:39:11 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -24,7 +24,7 @@ BROWSER_HEADERS = {
                   "(KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
     "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Encoding": "gzip, deflate",
     "Connection": "keep-alive",
     "Upgrade-Insecure-Requests": "1",
 }
